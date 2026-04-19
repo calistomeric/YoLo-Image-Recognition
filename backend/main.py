cd1c756f68fc330812b992f463254261da1c7614
calistomeric <okonkwocalistus98@gmail.com>
2026-04-19 18:02:02 +0100

    Day 3: Proper image upload handling and cleanup

--- backend/main.py
+++ backend/main.py
@@ -5,19 +5,22 @@ from model.inference import detect_objects
 
 app = FastAPI()
 
+upload_dir = "uploads"
+os.makedirs(upload_dir, exist_ok=True)
+
 @app.get("/")
 def home(): # Define a GET endpoint at the root URL
     return {"message": "CV object detection API is running!"}
 
 @app.post("/predict") # Define a POST endpoint for object detection
 async def predict(file: UploadFile = File(...)): # Accept an uploaded file as input
-    temp_path = f"temp_{file.filename}" # Create a temporary file path for the uploaded file
+    file_path = os.path.join(upload_dir, file.filename) # Create a temporary file path for the uploaded file
 
-    with open(temp_path, "wb") as buffer: # Open the temporary file in write-binary mode
+    with open(file_path, "wb") as buffer: # Open the temporary file in write-binary mode
         shutil.copyfileobj(file.file, buffer) # Copy the contents of the uploaded file to the temporary file
 
-    detections = detect_objects(temp_path) # Call the detect_objects function to perform object detection on the temporary file
+    detections = detect_objects(file_path) # Call the detect_objects function to perform object detection on the temporary file
 
-    os.remove(temp_path) # Remove the temporary file after processing
+    os.remove(file_path) # Remove the temporary file after processing
 
     return {"detections": detections} # Return the detected objects as a JSON response
